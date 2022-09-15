ecf8660dc46689e8ae9c3703c71964232efbc3f1
fukutaniryousuke <Fr83653554@gmail.com>
2022-09-15 11:18:29 +0900

    finish 10-3-1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2d/00d84ee39534001d150fa48441b472ab
@@ -0,0 +1,5 @@
+package jp.example;
+
+public class Thief {
+
+}
--- JavaPractice/src/main/java/jp/example/Thief.java
+++ JavaPractice/src/main/java/jp/example/Thief.java
@@ -1,5 +1,21 @@
 package jp.example;
 
 public class Thief {
-
+	String name;
+	int hp;
+	int mp;
+	
+	public Thief(String name, int hp, int mp) {
+		this.name = name;
+		this.hp = hp;
+		this.mp = mp;
+	}
+	
+	public Thief(String name, int hp) {
+		this(name, hp, 5);
+	}
+	
+	public Thief(String name) {
+		this(name, 40, 5);
+	}
 }
